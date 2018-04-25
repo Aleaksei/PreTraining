1b5fa951475fdf36619ca13fc4d0e34afd516f9d
Aleaksei <you@example.comshabunevich.aleaksei@gmail.com>
2018-04-25 17:27:46 +0300

    FirstCommit

---
+++ Algorithms/src/by/aleaksei/Task02.java
@@ -0,0 +1,23 @@
+package by.aleaksei;
+
+
+public class Task02 {
+
+    public static void main(String[] args) {
+        //int massDino = 35660;
+        //Algorithms.massDinosaur(massDino);
+        //int r1 = 7, r2 = 4;
+        //Algorithms.areaRing(r1,r2);
+        //int a = 6, b = 4;
+        //Algorithms.swapA_B(a,b);
+        //int numberFour = 7643;
+        //Algorithms.numberSequence(numberFour);
+        //int numberFiths = 12345;
+        //Algorithms.numberSumAndMultiplier(numberFiths);
+        //int numberSix = 123456;
+        //Algorithms.numberMean(numberSix);
+        //int numberSeven = 1234567;
+        //Algorithms.numberRevolt(numberSeven);
+    }
+
+}
