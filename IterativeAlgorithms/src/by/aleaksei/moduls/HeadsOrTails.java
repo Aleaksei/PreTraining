356d1a1a03b104201d845666bb35101bb80c11b0
Aleaksei <shabunevich.aleaksei@gmail.com>
2018-05-12 10:42:23 +0300

    Add files via upload

--- IterativeAlgorithms/src/by/aleaksei/moduls/HeadsOrTails.java
+++ IterativeAlgorithms/src/by/aleaksei/moduls/HeadsOrTails.java
@@ -8,20 +8,23 @@ import java.util.Random;
 
 public class HeadsOrTails {
 
-    public static void throwCoin(int numberShots) {
+    public static int throwCoin(int numberShots) {
 
-        int countHeads = 0, countTails = 0;
+        int countHeads = 0;
         Random random = new Random();
 
         for (int i = 0; i < numberShots; i++) {
-            if (random.nextInt() % 2 == 0) {
+            if (random.nextBoolean()) {
                 countHeads++;
-            } else {
-                countTails++;
             }
         }
-
-        System.out.printf("The coin was thrown %d times.\n Heads: %d.\n Tails: %d.", numberShots, countHeads, countTails);
+        return countHeads;
     }
 
+    public static void coinResulter(int numberShots) {
+
+        int countHeads = throwCoin(numberShots);
+        System.out.printf("The coin was thrown %d times.\n Heads: %d.\n Tails: %d.", numberShots,
+                countHeads, numberShots - countHeads);
+    }
 }
